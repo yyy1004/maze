d812d0ee4c360dfa0a1810a24b00e389b4daf03e
yyy1004 <3218253@qq.com>
2025-07-14 22:13:36 +0800

    加入ä¿star 和 target

--- game_manager.py
+++ game_manager.py
@@ -1,6 +1,8 @@
 import pygame
 from wall import Wall
 from player import Player
+from star import  Star
+from target import Target
 from utils.crash_collide import collided_rect
 
 
@@ -11,6 +13,9 @@ class GameManager:
 
         self.player = None
         self.walls = pygame.sprite.Group()
+        self.stars = pygame.sprite.Group()
+        self.stars_cnt = 0
+        self.targets = pygame.sprite.Group()
 
         self.load()
 
@@ -19,11 +24,23 @@ class GameManager:
         for x, y, width, height in walls:
             wall = Wall(x, y, width, height)
             wall.add(self.walls)
+    def load_stars(self, stars):
+        self.stars.empty()
+        for x, y in stars:
+            star = Star(x, y)
+            star.add(self.stars)
 
+    def load_targets(self, targets):
+        self.targets.empty()
+        for x, y in targets:
+            target = Target(x, y)
+            target.add(self.targets)
     def load_player(self, center_x, center_y, forward_angle):
         if self.player:
             self.player.kill()
         self.player = Player(center_x, center_y, forward_angle)
+
+
 
     def load(self):  # 加载地图信息
         with open("static/maps/level%d.txt" % self.level, 'r') as fin:
@@ -35,6 +52,19 @@ class GameManager:
 
             # 载入walls
             self.load_walls(walls)
+            self.stars_cnt = int(fin.readline())
+            starts = []
+            for i in range(self.stars_cnt):
+                x, y = map(int, fin.readline().split())
+                starts.append((x, y))
+            self.load_stars(starts)
+
+            targets_cnt = int(fin.readline())
+            targets = []
+            for i in range(targets_cnt):
+                x, y = map(int, fin.readline().split())
+                targets.append((x, y))
+            self.load_targets(targets)
 
             center_x, center_y, forward_angle = map(int, fin.readline().split())
             self.load_player(center_x, center_y, forward_angle)
@@ -51,4 +81,8 @@ class GameManager:
         self.player.update()
         self.screen.blit(self.player.image, self.player.rect)
 
+        self.stars.update()
+        self.stars.draw(self.screen)
+        self.targets.update()
+        self.targets.draw(self.screen)
 
