2aa3cc1bd4918b1b79c4863b71c901678233e40d
yyy1004 <3218253@qq.com>
2025-07-15 09:06:33 +0800

    通关 按任 意键退出

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import pygame
 import config
 from player import Player
 from game_manager import GameManager
-
+from utils.draw_text import draw_text
 
 pygame.init()
 pygame.mixer.init()  # 初始化声音
-screen = pygame.display.set_mode((config.SCREEN_WIDHT, config.SCREEN_EHIGHT))
+screen = pygame.display.set_mode((config.SCREEN_WIDTH, config.SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
 success_time = -1  # -1表示当前没有获胜，否则表示获胜的时刻
+success_finished = False  # 通关
 
 pygame.mixer.music.load("static/sounds/bgm.wav")
 pygame.mixer.music.set_volume(0.1)
@@ -23,21 +24,27 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif success_finished and event.type == pygame.KEYDOWN:  # 如果已通关，则按任意键结束
+            running = False
 
-    screen.fill("red")
-    # pygame.display.flip()
+    if success_finished:
+        screen.fill("black")
+        draw_text(screen, "Win!!!", 200, config.SCREEN_WIDTH / 2, config.SCREEN_HEIGHT / 2)
+    else:
+        screen.fill("red")
+        # pygame.display.flip()
 
-    # player.update()
-    if success_time >= 0:
-        if pygame.time.get_ticks() - success_time > 2000:  # 如果获胜后已经等待了2秒，则加载下一关
-            has_next = game_manager.next_level()
-            if not has_next:  # 如果没有下一关，则游戏结束
-                # success_finished = True
-                break
-            success_time = -1  # 将获胜时间清空
+        # player.update()
+        if success_time >= 0:
+            if pygame.time.get_ticks() - success_time > 2000:  # 如果获胜后已经等待了2秒，则加载下一关
+                has_next = game_manager.next_level()
+                if not has_next:  # 如果没有下一关，则游戏结束
+                    success_finished = True
+                    continue
+                success_time = -1  # 将获胜时间清空
 
-    if game_manager.update():
-        success_time = pygame.time.get_ticks()  # 更新获胜时刻
+        if game_manager.update():
+            success_time = pygame.time.get_ticks()  # 更新获胜时刻
 
     # screen.blit(player.image, player.rect)
 
